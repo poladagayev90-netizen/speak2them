eea738a4a1a7c9d6a16d38834bc0ccd1327c6218
Polad <poladagayev90gmail.com>
2026-07-03 03:19:18 +0400

    fix: change export default to module.exports for vercel api

--- api/analyze-call.js
+++ api/analyze-call.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // CORS setup if needed
   if (req.method === 'OPTIONS') {
     res.status(200).end();
